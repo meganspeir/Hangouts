9b48fd29e321330e5d3e90b6279afc003c080c27
Ni Mu <ni@nickelmu.com>
2013-10-26 01:55:19 -0700

    failed attempt to get bundles working

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.assets import Environment
+from webassets.loaders import PythonLoader
 
 app = Flask(__name__)
+assets = Environment(app)
+
+
+# Config
 app.config.from_object('config')
+
+
+# Load and register assets bundles.
+# bundles = PythonLoader('bundles').load_bundles()
+
+# for name, bundle in bundles.iteritems():
+#     assets.register(name, bundle)
+
+
+# Db
 db = SQLAlchemy(app)
 
+
+# MVC
 from app import views, models
---
+++ bundles.py
@@ -0,0 +1,49 @@
+# """
+# Define static webasset bundles.
+# """
+from flask.ext.assets import Bundle
+
+
+
+# all_css = Bundle(
+#     'css/bootstrap.css',
+#     'css/bootstrap-responsive.css',
+#     filters='cssmin',
+#     output='build/all.css'
+# )
+
+
+# # Bundle js libs, except jQuery (loads from CDN) and Modernizr (loads in head).
+# libs_js = Bundle(
+#     'js/libs/underscore.js',
+#     'js/libs/backbone.js',
+#     'js/libs/bootstrap.js',
+#     'js/libs/handlebars-1.0.0.beta.6.js',
+#     filters='uglifyjs',
+#     output='build/libs.js'
+# )
+
+# app_js = Bundle(
+#     Bundle(
+#         'js/app/*.coffee',
+#         filters='coffeescript',
+
+#         # Note: these can be eliminated once webassets 0.8 is released.
+#         output='build/coffee.js',
+#         debug=False
+#     ),
+#     filters='uglifyjs',
+#     output='build/app.js'
+# )
+
+app_js = Bundle(
+	Bundle(
+		'js/*.coffee',
+		filters='coffeescript',
+		output='build/coffee.js',
+		debug=True
+	),
+	filters='uglifyjs',
+	output='app.js'
+
+)
