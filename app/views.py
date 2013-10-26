556be4c0984d9d8ab2020ea28580c99819f8dbf6
Ni Mu <ni@nickelmu.com>
2013-10-26 01:01:39 -0700

    controller outline

--- app/views.py
+++ app/views.py
@@ -11,3 +11,42 @@ def index():
 # def login():
 
 
+# get / (index)
+# 	- if not logged in
+# 		=> prompt login (facebook)
+# 	- elif logged in && !avail
+# 		=> create hangout?
+# 		=> be available so your friends can hangout? (fomo fomo fomo)
+# 	- else (logged in & available)
+# 		=> (n friends are available, ) create a hangout?
+
+
+# post /login
+# 	- receive fb info
+# 	- if new user
+# 		=> persist in db
+# 	- else
+# 		=> update any change data
+
+# 	save session (so they don't have to login again)
+
+# 	redirect to index
+
+
+# post /logout
+# 	clear session
+
+
+
+# get /hangout/new # create a new thing, and choose an activity/location
+# post /hangout/new # process the new thing that this is
+
+# get /hangout/{{id}} # show the hangout
+
+# get /hangout/{{id}}/available_friends # list friends within radius of hangout location
+
+# post /hangout/{{id}}/propose_alternative <-- i'm a responder and i don't like your activity?
+
+# get /hangout/{{id}}/edit # change details and try to reinvite
+
+# post /hangout/{{id}}/invite
